be7b09f2d52399f11bbf66288a70962fea1878c6
anastasiak2512 <anastasia.kazakova2512@gmail.com>
2014-03-25 01:01:17 +0400

    kiwi tests

---
+++ KiwiLiveTempl.m
@@ -0,0 +1,13 @@
+#import "KiwiMacros.h"
+#import "KWSpec.h"
+#import "KWExample.h"
+
+
+SPEC_BEGIN(AppTestSpec)
+    describe(@"AppTest", ^{
+
+        it(@"it", ^{
+            
+        });
+    });
+SPEC_END
