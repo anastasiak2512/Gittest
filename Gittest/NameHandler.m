e90b323063154f76a63b063441550330cf05c8d9
anastasiak2512 <anastasia.kazakova2512@gmail.com>
2014-07-22 16:18:04 +0400

    revert

--- Gittest/NameHandler.m
+++ Gittest/NameHandler.m
@@ -4,28 +4,58 @@
 //
 
 #import "NameHandler.h"
-
+#import <AppKit/NSColor.h>
 
 @implementation NameHandler {
          //tt
     //rrr
+
 }
+
 - (void)say_hello {
     printf("Hello, %s\n", [self.name UTF8String]);
 }
 
+//ask_question and give_answer
+//TODO: second
 - (void)ask_question:(NSString *)question {
     printf("%s, %s\n", [question UTF8String], [self.name UTF8String]);
 }
 
 
+/**
+* Doc comment
+* @param answer
+*/
 - (void)give_answer:(NSString *)answer {
+    //TODO: first
     printf("%s, you answer is: %s\n", [self.name UTF8String], [answer UTF8String]);
 }
 
 - (void)say_bye {
     printf("Bye, %s\n", [self.name UTF8String]);
+
 }
 
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
