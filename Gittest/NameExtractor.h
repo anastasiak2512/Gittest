4793da138c31e34f03ab9da89083d55a60a743c0
anastasiak2512 <anastasia.kazakova2512@gmail.com>
2014-07-24 17:15:39 +0400

    new files

---
+++ Gittest/NameExtractor.h
@@ -0,0 +1,10 @@
+//
+// Created by AK on 24/07/14.
+// Copyright (c) 2014 JB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NameExtractor : NSObject
+@end
---
+++ Gittest/NameExtractor.m
@@ -0,0 +1,12 @@
+//
+// Created by AK on 24/07/14.
+// Copyright (c) 2014 JB. All rights reserved.
+//
+
+#import "NameExtractor.h"
+
+
+@implementation NameExtractor {
+
+}
+@end
