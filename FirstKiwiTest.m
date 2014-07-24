2f9a71221e89772932ceb792dba734fe456b3935
anastasiak2512 <anastasia.kazakova2512@gmail.com>
2014-07-24 19:14:05 +0400

    kiwi test

--- FirstKiwiTest.m
+++ FirstKiwiTest.m
@@ -9,13 +9,8 @@
 #import "Kiwi.h"
 
 
-SPEC_BEGIN(MathSpec)
-    describe(@"Math", ^{
-        it(@"is pretty cool", ^{
-            NSUInteger a = 16;
-            NSUInteger b = 26;
-            [[theValue(a + b) should] equal:theValue(42)];
-        });
+SPEC_BEGIN(MeteorSpec)
+    describe(@"Meteor", ^{
 
     });
 SPEC_END
